e1dc28906968e71e9f3ded326d20aec5be6d976f
Torben Ewert <torben@ewert-online.com>
2022-12-28 17:19:55 +0100

    ...

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+prefix="$1"
+cmake="$2"
+os="$3"
+
+
+if [[ $os = "windows" ]]
+then
+  "$cmake" \
+    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_INSTALL_PREFIX="$(cygpath $prefix)" \
+    -DCMAKE_MAKE_PROGRAM=make \
+    -GUnix Makefiles \
+    -DCMAKE_HOST_WIN32=true \
+    .
+else
+  "$cmake" \
+    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_INSTALL_PREFIX="$prefix" \
+    .
+fi
